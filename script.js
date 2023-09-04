84bec7129417b1d60ae1bd573d7c35289515fa6b
Anton Novak <anton-novak@outlook.com>
2023-09-04 16:41:20 +0100

    refactor for testing purposes

--- script.js
+++ script.js
@@ -1,45 +1,46 @@
 // to test on the actual device if .js file loads up correctly
+// it does not :(
 let a = document.createElement("h1");
 a.innerHTML = "Hello";
 document.querySelector("body").prepend(a);
 
-function stopOtherStations(currentAudioElement) {
-    let audioElements = document.getElementsByTagName("audio");
-    Array.from(audioElements).forEach(element => {
-        if (element !== currentAudioElement) element.pause();
-    });
-};
+// function stopOtherStations(currentAudioElement) {
+//     let audioElements = document.getElementsByTagName("audio");
+//     Array.from(audioElements).forEach(element => {
+//         if (element !== currentAudioElement) element.pause();
+//     });
+// };
 
-async function getAsebeiaNowPlaying() {
-    let response = await fetch("https://azura.asebeia.su/api/nowplaying/1");
-    let nowPlaying = await response.json();
-    return nowPlaying.now_playing.song.text;
-};
+// async function getAsebeiaNowPlaying() {
+//     let response = await fetch("https://azura.asebeia.su/api/nowplaying/1");
+//     let nowPlaying = await response.json();
+//     return nowPlaying.now_playing.song.text;
+// };
 
-window.onload = function (event) {
-    let audioElements = document.getElementsByTagName("audio");
-    Array.from(audioElements).forEach(element => {
-        element.addEventListener("play", (event) => {
-            stopOtherStations(element);
-        });
-    });
+// window.onload = function (event) {
+//     let audioElements = document.getElementsByTagName("audio");
+//     Array.from(audioElements).forEach(element => {
+//         element.addEventListener("play", (event) => {
+//             stopOtherStations(element);
+//         });
+//     });
 
-    // asebeia-specific code
-    let audioAsebeia = document.getElementById("asebeia-station");
-    let intervalId;
+//     // asebeia-specific code
+//     let audioAsebeia = document.getElementById("asebeia-station");
+//     let intervalId;
 
-    audioAsebeia.addEventListener("playing", async (event) => {
-        let nowPlayingAsebeia = document.getElementById("now-playing-asebeia");
-        nowPlayingAsebeia.innerHTML = `Now playing: ${await getAsebeiaNowPlaying()}`;
-        intervalId = setInterval(async () => {
-            console.log(await getAsebeiaNowPlaying());
-            nowPlayingAsebeia.innerHTML = `Now playing: ${await getAsebeiaNowPlaying()}`;
-        }, 10000);
-    });
+//     audioAsebeia.addEventListener("playing", async (event) => {
+//         let nowPlayingAsebeia = document.getElementById("now-playing-asebeia");
+//         nowPlayingAsebeia.innerHTML = `Now playing: ${await getAsebeiaNowPlaying()}`;
+//         intervalId = setInterval(async () => {
+//             console.log(await getAsebeiaNowPlaying());
+//             nowPlayingAsebeia.innerHTML = `Now playing: ${await getAsebeiaNowPlaying()}`;
+//         }, 10000);
+//     });
 
-    audioAsebeia.addEventListener("pause", (event) => {
-        clearInterval(intervalId);
-    });
+//     audioAsebeia.addEventListener("pause", (event) => {
+//         clearInterval(intervalId);
+//     });
 
 };
 
