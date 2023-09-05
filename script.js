646e35986a20b0cad4d43dbc37ba5b0402628377
Anton Novak <anton-novak@outlook.com>
2023-09-05 23:43:22 +0100

    feat: refactor fetch to XML

--- script.js
+++ script.js
@@ -14,12 +14,20 @@ function playPause(audioElement) {
     };
 };
 
-// function getAsebeiaNowPlaying() {
-//     return fetch("https://azura.asebeia.su/api/nowplaying/1")
-//         .then((response) => {
-//             return response.json();
-//         });
-// };
+// fetch is not compatible with gecko 38
+
+function getAsebeiaNowPlaying(callback) {
+    const xhr = new XMLHttpRequest();
+
+    xhr.onreadystatechange = () => {
+        if (xhr.readyState === 4) {
+            callback(xhr.response);
+        }
+    };
+
+    xhr.open("GET", "https://azura.asebeia.su/api/nowplaying/1", true);
+    xhr.send();
+};
 
 window.onload = function (event) {
     // stopping other stations when starting playback of a new one
@@ -39,28 +47,28 @@ window.onload = function (event) {
     });
 
     // get track info from asebeia
-    // let audioAsebeia = document.getElementById("asebeia-station");
-    // let nowPlayingAsebeia = document.createElement("p");
-    // let intervalId;
+    let audioAsebeia = document.getElementById("asebeia-station");
+    let nowPlayingAsebeia = document.createElement("p");
+    let intervalId;
 
-    // audioAsebeia.addEventListener("playing", async (event) => {
-    //     document.getElementById("now-playing").appendChild(nowPlayingAsebeia);
-    //     getAsebeiaNowPlaying()
-    //         .then((data) => {
-    //             nowPlayingAsebeia.innerHTML = `Now playing: ${data.now_playing.song.text}`;
-    //         });
-    //     intervalId = setInterval(() => {
-    //         getAsebeiaNowPlaying()
-    //             .then((data) => {
-    //                 nowPlayingAsebeia.innerHTML = `Now playing: ${data.now_playing.song.text}`;
-    //             });
-    //     }, 10000);
-    // });
+    audioAsebeia.addEventListener("playing", async (event) => {
+        document.getElementById("now-playing").appendChild(nowPlayingAsebeia);
+        getAsebeiaNowPlaying((data) => {
+            data = JSON.parse(data);
+            nowPlayingAsebeia.innerHTML = `Now playing: ${data.now_playing.song.text}`;
+        });
+        intervalId = setInterval(() => {
+            getAsebeiaNowPlaying((data) => {
+                data = JSON.parse(data);
+                nowPlayingAsebeia.innerHTML = `Now playing: ${data.now_playing.song.text}`;
+            });
+        }, 10000);
+    });
 
-    // audioAsebeia.addEventListener("pause", (event) => {
-    //     clearInterval(intervalId);
-    //     nowPlayingAsebeia.remove();
-    // });
+    audioAsebeia.addEventListener("pause", (event) => {
+        clearInterval(intervalId);
+        nowPlayingAsebeia.remove();
+    });
 };
 
 // scraping HTML of https://www.silver.ru and other websites for tags
